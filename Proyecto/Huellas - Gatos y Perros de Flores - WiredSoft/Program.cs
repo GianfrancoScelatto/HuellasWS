32efd3a1e889ef2c859d8a9dd2c77de64d381d85
Gian Franco Scelatto <52085451+GianfrancoScelatto@users.noreply.github.com>
2020-08-07 14:06:39 -0300

    Add files via upload

---
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Huellas___Gatos_y_Perros_de_Flores___WiredSoft
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
