17721cd8a643a466665fa7cbbbf86aa43a6f2805
Rodrigo Paucara <rodriogpaucarat@gmail.com>
2020-08-27 20:19:24 -0300

    Recuperar Contraseña

---
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Recuperar.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Huellas___Gatos_y_Perros_de_Flores___WiredSoft
+{
+    partial class Recuperar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.cbbPregunta = new System.Windows.Forms.ComboBox();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtValidarContraseña = new System.Windows.Forms.TextBox();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 19);
+            this.label2.TabIndex = 62;
+            this.label2.Text = "Pregunta de seguridad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 19);
+            this.label1.TabIndex = 61;
+            this.label1.Text = "Ingrese su usuario: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 19);
+            this.label3.TabIndex = 64;
+            this.label3.Text = "Nueva contraseña:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 19);
+            this.label4.TabIndex = 63;
+            this.label4.Text = "Respuesta:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 19);
+            this.label5.TabIndex = 65;
+            this.label5.Text = "Validar contraseña:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(252, 46);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(187, 20);
+            this.txtUser.TabIndex = 66;
+            this.txtUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cbbPregunta
+            // 
+            this.cbbPregunta.FormattingEnabled = true;
+            this.cbbPregunta.Location = new System.Drawing.Point(252, 86);
+            this.cbbPregunta.Name = "cbbPregunta";
+            this.cbbPregunta.Size = new System.Drawing.Size(187, 21);
+            this.cbbPregunta.TabIndex = 67;
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(252, 128);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(187, 20);
+            this.txtRespuesta.TabIndex = 68;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(252, 169);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(187, 20);
+            this.txtContraseña.TabIndex = 69;
+            // 
+            // txtValidarContraseña
+            // 
+            this.txtValidarContraseña.Location = new System.Drawing.Point(252, 210);
+            this.txtValidarContraseña.Name = "txtValidarContraseña";
+            this.txtValidarContraseña.Size = new System.Drawing.Size(187, 20);
+            this.txtValidarContraseña.TabIndex = 70;
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIngresar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnIngresar.Location = new System.Drawing.Point(157, 288);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(130, 56);
+            this.BtnIngresar.TabIndex = 71;
+            this.BtnIngresar.Text = "Guardar cambios";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // Recuperar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(453, 356);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.txtValidarContraseña);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.cbbPregunta);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Recuperar";
+            this.Text = "Recuperar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.ComboBox cbbPregunta;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtValidarContraseña;
+        private System.Windows.Forms.Button BtnIngresar;
+    }
+}
