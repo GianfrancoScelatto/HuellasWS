48b1a27859b3a8dac5fc365b771bd0df992112c1
Rodrigo Paucara <rodrigopaucarat@gmail.com>
2020-09-06 22:48:57 -0300

    0

--- Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/ListarVacuna.Designer.cs
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/ListarVacuna.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListarVacuna));
+            this.panelBotones = new System.Windows.Forms.Panel();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.chxFallecidos = new System.Windows.Forms.CheckBox();
+            this.chxTransito = new System.Windows.Forms.CheckBox();
+            this.chxNoAdoptados = new System.Windows.Forms.CheckBox();
+            this.chxAdoptados = new System.Windows.Forms.CheckBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.panelListar = new System.Windows.Forms.Panel();
+            this.dgwVacunas = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Animal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaRenovacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelBotones.SuspendLayout();
+            this.panelListar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwVacunas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBotones
+            // 
+            this.panelBotones.Controls.Add(this.btnExportar);
+            this.panelBotones.Controls.Add(this.btnEliminar);
+            this.panelBotones.Controls.Add(this.btnNuevo);
+            this.panelBotones.Controls.Add(this.btnModificar);
+            this.panelBotones.Controls.Add(this.chxFallecidos);
+            this.panelBotones.Controls.Add(this.chxTransito);
+            this.panelBotones.Controls.Add(this.chxNoAdoptados);
+            this.panelBotones.Controls.Add(this.chxAdoptados);
+            this.panelBotones.Controls.Add(this.txtBuscar);
+            this.panelBotones.Controls.Add(this.lblBuscar);
+            this.panelBotones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBotones.Location = new System.Drawing.Point(0, 0);
+            this.panelBotones.Name = "panelBotones";
+            this.panelBotones.Size = new System.Drawing.Size(1067, 55);
+            this.panelBotones.TabIndex = 28;
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExportar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExportar.BackgroundImage")));
+            this.btnExportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportar.Location = new System.Drawing.Point(864, 5);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(39, 40);
+            this.btnExportar.TabIndex = 47;
+            this.btnExportar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEliminar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEliminar.BackgroundImage")));
+            this.btnEliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Location = new System.Drawing.Point(1010, 5);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(40, 40);
+            this.btnEliminar.TabIndex = 46;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNuevo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNuevo.BackgroundImage")));
+            this.btnNuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.Location = new System.Drawing.Point(918, 5);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(40, 40);
+            this.btnNuevo.TabIndex = 45;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnModificar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnModificar.BackgroundImage")));
+            this.btnModificar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Location = new System.Drawing.Point(964, 5);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(40, 40);
+            this.btnModificar.TabIndex = 44;
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // chxFallecidos
+            // 
+            this.chxFallecidos.AutoSize = true;
+            this.chxFallecidos.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chxFallecidos.ForeColor = System.Drawing.SystemColors.Control;
+            this.chxFallecidos.Location = new System.Drawing.Point(371, 29);
+            this.chxFallecidos.Name = "chxFallecidos";
+            this.chxFallecidos.Size = new System.Drawing.Size(90, 21);
+            this.chxFallecidos.TabIndex = 43;
+            this.chxFallecidos.Text = "Fallecidos";
+            this.chxFallecidos.UseVisualStyleBackColor = true;
+            // 
+            // chxTransito
+            // 
+            this.chxTransito.AutoSize = true;
+            this.chxTransito.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chxTransito.ForeColor = System.Drawing.SystemColors.Control;
+            this.chxTransito.Location = new System.Drawing.Point(371, 5);
+            this.chxTransito.Name = "chxTransito";
+            this.chxTransito.Size = new System.Drawing.Size(94, 21);
+            this.chxTransito.TabIndex = 42;
+            this.chxTransito.Text = "En transito";
+            this.chxTransito.UseVisualStyleBackColor = true;
+            // 
+            // chxNoAdoptados
+            // 
+            this.chxNoAdoptados.AutoSize = true;
+            this.chxNoAdoptados.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chxNoAdoptados.ForeColor = System.Drawing.SystemColors.Control;
+            this.chxNoAdoptados.Location = new System.Drawing.Point(235, 29);
+            this.chxNoAdoptados.Name = "chxNoAdoptados";
+            this.chxNoAdoptados.Size = new System.Drawing.Size(123, 21);
+            this.chxNoAdoptados.TabIndex = 41;
+            this.chxNoAdoptados.Text = "No adoptados";
+            this.chxNoAdoptados.UseVisualStyleBackColor = true;
+            // 
+            // chxAdoptados
+            // 
+            this.chxAdoptados.AutoSize = true;
+            this.chxAdoptados.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chxAdoptados.ForeColor = System.Drawing.SystemColors.Control;
+            this.chxAdoptados.Location = new System.Drawing.Point(235, 5);
+            this.chxAdoptados.Name = "chxAdoptados";
+            this.chxAdoptados.Size = new System.Drawing.Size(100, 21);
+            this.chxAdoptados.TabIndex = 40;
+            this.chxAdoptados.Text = "Adoptados";
+            this.chxAdoptados.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(76, 11);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(136, 20);
+            this.txtBuscar.TabIndex = 39;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblBuscar.Location = new System.Drawing.Point(17, 12);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(53, 17);
+            this.lblBuscar.TabIndex = 38;
+            this.lblBuscar.Text = "Buscar:";
+            // 
+            // panelListar
+            // 
+            this.panelListar.Controls.Add(this.dgwVacunas);
+            this.panelListar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelListar.Location = new System.Drawing.Point(0, 55);
+            this.panelListar.Name = "panelListar";
+            this.panelListar.Size = new System.Drawing.Size(1067, 576);
+            this.panelListar.TabIndex = 29;
+            // 
+            // dgwVacunas
+            // 
+            this.dgwVacunas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwVacunas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Animal,
+            this.Descripcion,
+            this.FechaRenovacion});
+            this.dgwVacunas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgwVacunas.Location = new System.Drawing.Point(0, 0);
+            this.dgwVacunas.Name = "dgwVacunas";
+            this.dgwVacunas.Size = new System.Drawing.Size(1067, 576);
+            this.dgwVacunas.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id Vacuna";
+            this.Column1.Name = "Column1";
+            // 
+            // Animal
+            // 
+            this.Animal.HeaderText = "Para animal";
+            this.Animal.Name = "Animal";
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripción";
+            this.Descripcion.Name = "Descripcion";
+            // 
+            // FechaRenovacion
+            // 
+            this.FechaRenovacion.HeaderText = "Fecha de Renovacion";
+            this.FechaRenovacion.Name = "FechaRenovacion";
+            // 
+            // ListarVacuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(1067, 631);
+            this.Controls.Add(this.panelListar);
+            this.Controls.Add(this.panelBotones);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "ListarVacuna";
             this.Text = "ListarVacuna";
+            this.panelBotones.ResumeLayout(false);
+            this.panelBotones.PerformLayout();
+            this.panelListar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwVacunas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelBotones;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.CheckBox chxFallecidos;
+        private System.Windows.Forms.CheckBox chxTransito;
+        private System.Windows.Forms.CheckBox chxNoAdoptados;
+        private System.Windows.Forms.CheckBox chxAdoptados;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.Panel panelListar;
+        private System.Windows.Forms.DataGridView dgwVacunas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Animal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaRenovacion;
     }
 }
--- Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/ListarVacuna.cs
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/ListarVacuna.cs
@@ -16,5 +16,12 @@ namespace Huellas___Gatos_y_Perros_de_Flores___WiredSoft
         {
             InitializeComponent();
         }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Form Vacunas = new Vacunas();
+            Vacunas.Show();
+
+        }
     }
 }
