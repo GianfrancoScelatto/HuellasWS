13d13734a6ad7131239e24a97bba17210d63bcaa
Rodrigo Paucara <rodriogpaucarat@gmail.com>
2020-09-23 00:08:46 -0300

    Update Vacunas BR

--- Proyecto/BusinessRules/BR_Vacunas.cs
+++ Proyecto/BusinessRules/BR_Vacunas.cs
@@ -7,7 +7,7 @@ using System.Data;
 using DataAccess;
 namespace BusinessRules
 {
-   public  class BR_Vacunas
+    public class BR_Vacunas
     {
         private DA_Vacunas VacunasDA = new DA_Vacunas();
 
@@ -23,5 +23,21 @@ namespace BusinessRules
         {
             VacunasDA.AM_Vacunas(IdVacuna, Vacuna, IdEspecie, FrecuenciaVacunacion, Descripcion);
         }
+
+        public void Eliminar_Vacuna(int IdVacuna, string Vacuna, int IdEspecie, string FrecuenciaVacunacion, string Descripcion)
+        {
+            VacunasDA.BajaVacuna(IdVacuna, Vacuna, IdEspecie, FrecuenciaVacunacion, Descripcion);
+
+        }
+
+
+
     }
+
 }
+
+
+
+        
+        
+   
