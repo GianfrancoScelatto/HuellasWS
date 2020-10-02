2ca585525a0ae6e68d441acdaaa3e7af04af0b0f
matiasnociti <matinociti98@gmail.com>
2020-10-01 23:34:45 -0300

    BR Persona y Da Persona Modificar

---
+++ Proyecto/BusinessRules/BR_Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DataAccess;
+namespace BusinessRules
+{
+    public class BR_Persona
+    {
+        readonly DA_Persona ObjPersona = new DA_Persona();
+
+        public static DataTable ListarPersona()
+        {
+        }
+        public void AltaPersona(int idPersona, int IdTipoPersona, string Nombre, string Apellido, int Edad, int DNI, string Domicilio, string Localidad, int Codigo_Postal, string Calles, int Altura, string Sexo, int Telefono, int Celular, string Email, string UsuarioFaceIg, bool ListaNegra, string Motivo, byte Deshabilitado)
+        {
+            ObjPersona.AltaPersona(idPersona, IdTipoPersona, Nombre, Apellido, Edad, DNI, Domicilio, Localidad, Codigo_Postal, Calles, Altura, Sexo, Telefono, Celular, Email, UsuarioFaceIg, ListaNegra, Motivo,Deshabilitado)
+        }
+        public void ModificarPersona(int idPersona, int IdTipoPersona, string Nombre, string Apellido, int Edad, int DNI, string Domicilio, string Localidad, int Codigo_Postal, string Calles, int Altura, string Sexo, int Telefono, int Celular, string Email, string UsuarioFaceIg, bool ListaNegra, string Motivo, byte Deshabilitado)
+        {
+            ObjPersona.ModificarPersona(idPersona, IdTipoPersona, Nombre, Apellido, Edad, DNI, Domicilio, Localidad, Codigo_Postal, Calles, Altura, Sexo, Telefono, Celular, Email, UsuarioFaceIg, ListaNegra, Motivo, Deshabilitado);
+        }
+        public void BorrarPersona(int idPersona, int IdUsuario, int IdMovimiento, bool Deshabilitado, string Descripcion)
+        {
+            ObjPersona.BajaPersona(idPersona, IdUsuario, IdMovimiento,Deshabilitado, Descripcion);
+        }
+        public void FiltrarPersona(string Busqueda)
+        {
+            ObjPersona.FiltrarPersona(Busqueda);
+        }
+    }
+}
--- Proyecto/DataAccess/DA_Persona.cs
+++ Proyecto/DataAccess/DA_Persona.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccess
 {
-    public class DA_Transitante : DA_Connection
+    public class DA_Persona : DA_Connection
     {
-        public DataTable ListarAnimal()
+        public DataTable ListarPersona()
         {
             using (var connection = GetConnection())
             {
@@ -19,7 +19,7 @@ namespace DataAccess
                 using (var command = new SqlCommand())
                 {
                     command.Connection = connection;
-                    command.CommandText = "prc_Historial";
+                    command.CommandText = "prc_PersonaHistorial";
                     command.CommandType = CommandType.StoredProcedure;
                     SqlDataReader leer = command.ExecuteReader();
                     tabla.Load(leer);
@@ -28,7 +28,7 @@ namespace DataAccess
                 }
             }
         }
-        public void AltaPersona(int idPersona,int IdTipoPersona, string Nombre, string Apellido, int Edad, int DNI,string Domicilio ,string Localidad, int Codigo_Postal, string Calles, int Altura,string Sexo, int Telefono, int Celular, string Email,string UsuarioFaceIg, bool ListaNegra, string Motivo,byte Deshabilitado)
+        public void AltaPersona(int idPersona, int IdTipoPersona, string Nombre, string Apellido, int Edad, int DNI, string Domicilio, string Localidad, int Codigo_Postal, string Calles, int Altura, string Sexo, int Telefono, int Celular, string Email, string UsuarioFaceIg, bool ListaNegra, string Motivo, byte Deshabilitado)
         {
             using (var connection = GetConnection())
             {
@@ -51,18 +51,51 @@ namespace DataAccess
                     command.Parameters.AddWithValue(" @Telefono", Telefono);
                     command.Parameters.AddWithValue("@Celular", Celular);
                     command.Parameters.AddWithValue("@Email", Email);
-                    command.Parameters.AddWithValue("@UsuarioFaceIg",UsuarioFaceIg);
-                    command.Parameters.AddWithValue("@ListaNegra",ListaNegra);
+                    command.Parameters.AddWithValue("@UsuarioFaceIg", UsuarioFaceIg);
+                    command.Parameters.AddWithValue("@ListaNegra", ListaNegra);
                     command.Parameters.AddWithValue("@Motivo", Motivo);
                     command.Parameters.AddWithValue("@Deshabilitado", Deshabilitado);
                     command.CommandText = "prc_AltaPersona";
-                    //falta agregar los que van al historial, falta crear el de modificar. y la BR y dsp funcionalidad, a su vez tambien faltan los prc en Base Datos
+                    //y la BR y dsp funcionalidad, a su vez tambien faltan los prc en Base Datos
                     command.CommandType = CommandType.StoredProcedure;
                     command.ExecuteNonQuery();
                 }
             }
         }
-        public void BajaPersona(int IdPersona, int IdUsuario, int IdMovimiento,bool Deshabilitado)
+            public void ModificarPersona(int idPersona, int IdTipoPersona, string Nombre, string Apellido, int Edad, int DNI, string Domicilio, string Localidad, int Codigo_Postal, string Calles, int Altura, string Sexo, int Telefono, int Celular, string Email, string UsuarioFaceIg, bool ListaNegra, string Motivo, byte Deshabilitado)
+        {
+                using (var connection = GetConnection())
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.Parameters.AddWithValue("@idAdoptante", idPersona);
+                    command.Parameters.AddWithValue("@TipoPersona", IdTipoPersona);
+                    command.Parameters.AddWithValue("@Nombre", Nombre);
+                    command.Parameters.AddWithValue("@Apellido", Apellido);
+                    command.Parameters.AddWithValue("@Edad", Edad);
+                    command.Parameters.AddWithValue("@DNI", DNI);
+                    command.Parameters.AddWithValue("@Domicilio", Domicilio);
+                    command.Parameters.AddWithValue("@Localidad", Localidad);
+                    command.Parameters.AddWithValue("@Codigo_Postal", Codigo_Postal);
+                    command.Parameters.AddWithValue("@Calles", Calles);
+                    command.Parameters.AddWithValue("@Altura", Altura);
+                    command.Parameters.AddWithValue("@Sexo", Sexo);
+                    command.Parameters.AddWithValue(" @Telefono", Telefono);
+                    command.Parameters.AddWithValue("@Celular", Celular);
+                    command.Parameters.AddWithValue("@Email", Email);
+                    command.Parameters.AddWithValue("@UsuarioFaceIg", UsuarioFaceIg);
+                    command.Parameters.AddWithValue("@ListaNegra", ListaNegra);
+                    command.Parameters.AddWithValue("@Motivo", Motivo);
+                    command.Parameters.AddWithValue("@Deshabilitado", Deshabilitado);
+                    command.CommandText = "prc_ModificarPersona";
+                    command.CommandType = CommandType.StoredProcedure;
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        public void BajaPersona(int IdPersona, int IdUsuario, int IdMovimiento, bool Deshabilitado, string Descripcion)
         {
             using (var connection = GetConnection())
             {
@@ -75,7 +108,7 @@ namespace DataAccess
                     command.Parameters.AddWithValue("@IdUsuario", IdUsuario);
                     command.Parameters.AddWithValue("@IdMovimiento", IdMovimiento);
                     command.Parameters.AddWithValue("@Deshabilitado", Deshabilitado);
-                    command.Parameters.AddWithValue("@Descripcion", IdPersona);
+                    command.Parameters.AddWithValue("@Descripcion", Descripcion);
                     command.CommandText = "prc_BajaPersona";
                     command.CommandType = CommandType.StoredProcedure;
                     command.ExecuteNonQuery();
@@ -83,7 +116,7 @@ namespace DataAccess
             }
         }
 
-        public DataTable FiltrarTransitante(string busqueda)
+        public DataTable FiltrarPersona(string busqueda)
         {
             using (var connection = GetConnection())
             {
@@ -93,7 +126,7 @@ namespace DataAccess
                 {
                     command.Connection = connection;
                     command.Parameters.AddWithValue("@Busqueda", busqueda);
-                    command.CommandText = "prc_Busqueda_Transitante";
+                    command.CommandText = "prc_FiltrarPersona";
                     command.CommandType = CommandType.StoredProcedure;
                     SqlDataReader reader = command.ExecuteReader();
                     tabla.Load(reader);
