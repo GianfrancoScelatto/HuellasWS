32d5293daf4dd6f69925ad70eaa033eb42ce73f2
Rodrigo Paucara <rodriogpaucarat@gmail.com>
2020-10-06 23:08:46 -0300

    DA BR y Entidades Establecimiento

---
+++ Proyecto/BusinessRules/BR_Establecimiento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DataAccess;
+namespace BusinessRules
+{
+    public class BR_Establecimiento
+    {
+        private DA_Establecimiento EstablecimientoDA = new DA_Establecimiento();
+
+        public DataTable MostrarEstablecimiento()
+        {
+
+            DataTable tabla = new DataTable();
+            tabla = EstablecimientoDA.ListarEstablecimiento();
+            return tabla;
+        }
+
+        public void AltaEstablecimiento(int TipoEstablecimiento, string Nombre, string HorarioAtencion, string Localidad, string CodigoPostal, string Calle, int Altura, bool Internacion)
+        {
+            EstablecimientoDA.AltaEstablecimiento(TipoEstablecimiento, Nombre, HorarioAtencion, Localidad, CodigoPostal, Calle, Altura, Internacion);
+        }
+
+        public void ModifcarEstablecimiento(int IdEstablecimiento, int TipoEstablecimiento, string Nombre, string HorarioAtencion, string Localidad, string CodigoPostal, string Calle, int Altura, bool Internacion)
+        {
+            EstablecimientoDA.ModificarEstablecimiento(IdEstablecimiento, TipoEstablecimiento, Nombre, HorarioAtencion, Localidad, CodigoPostal, Calle, Altura, Internacion);
+        }
+
+        public void BajaEstablecimiento(int IdEstablecimiento, bool Deshabilitado)
+        {
+            EstablecimientoDA.BajaVacuna(IdEstablecimiento, Deshabilitado);
+        }
+    }
+}
---
+++ Proyecto/Entities/E_Establecimiento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class E_Establecimiento
+    {
+        public int IdEstablecimiento { get; set; }
+        public int IdTipoEstablecimiento { get; set; }
+        public string Nombre { get; set; }
+        public string HorarioAtencion { get; set; }
+        public string Localidad { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+        public bool Internacion { get; set; }
+        public bool Deshabilitado { get; set; }
+
+    }
+}
