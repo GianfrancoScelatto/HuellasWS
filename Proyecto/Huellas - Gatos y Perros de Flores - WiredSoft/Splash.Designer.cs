1ac6dd9164bac25f189d23909818281e99b3e6a3
Rodrigo Paucara <rodriogpaucarat@gmail.com>
2020-11-07 17:17:30 -0300

    Update Splash

---
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Splash.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Huellas___Gatos_y_Perros_de_Flores___WiredSoft
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+            this.picSplash = new System.Windows.Forms.PictureBox();
+            this.panelBar = new System.Windows.Forms.Panel();
+            this.panelProgreso = new System.Windows.Forms.Panel();
+            this.timerProgreso = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picSplash)).BeginInit();
+            this.panelBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picSplash
+            // 
+            this.picSplash.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.picSplash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picSplash.Image = ((System.Drawing.Image)(resources.GetObject("picSplash.Image")));
+            this.picSplash.Location = new System.Drawing.Point(0, 0);
+            this.picSplash.Name = "picSplash";
+            this.picSplash.Size = new System.Drawing.Size(684, 361);
+            this.picSplash.TabIndex = 0;
+            this.picSplash.TabStop = false;
+            // 
+            // panelBar
+            // 
+            this.panelBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(39)))), ((int)(((byte)(105)))));
+            this.panelBar.Controls.Add(this.panelProgreso);
+            this.panelBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.panelBar.Location = new System.Drawing.Point(0, 338);
+            this.panelBar.Name = "panelBar";
+            this.panelBar.Size = new System.Drawing.Size(684, 23);
+            this.panelBar.TabIndex = 1;
+            // 
+            // panelProgreso
+            // 
+            this.panelProgreso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(81)))), ((int)(((byte)(135)))));
+            this.panelProgreso.Location = new System.Drawing.Point(4, 3);
+            this.panelProgreso.Name = "panelProgreso";
+            this.panelProgreso.Size = new System.Drawing.Size(87, 19);
+            this.panelProgreso.TabIndex = 0;
+            // 
+            // timerProgreso
+            // 
+            this.timerProgreso.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.panelBar);
+            this.Controls.Add(this.picSplash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            ((System.ComponentModel.ISupportInitialize)(this.picSplash)).EndInit();
+            this.panelBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSplash;
+        private System.Windows.Forms.Panel panelBar;
+        private System.Windows.Forms.Panel panelProgreso;
+        private System.Windows.Forms.Timer timerProgreso;
+    }
+}
---
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Splash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Huellas___Gatos_y_Perros_de_Flores___WiredSoft
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panelProgreso.Width += 3;
+            if (panelProgreso.Width >700)
+            {
+                timerProgreso.Stop();
+                MenuInicial menu = new MenuInicial();
+                menu.Show();
+                this.Hide();
+
+            }
+        }
+    }
+}
