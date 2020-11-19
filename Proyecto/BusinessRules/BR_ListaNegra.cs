f0e00ce08645994587ead3784fb25d53b8c0001d
Rodrigo <52087673+DefaultArg@users.noreply.github.com>
2020-11-19 19:30:43 -0300

    Update DA Lista Negra

--- Proyecto/BusinessRules/BR_ListaNegra.cs
+++ Proyecto/BusinessRules/BR_ListaNegra.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
+using System.Data;
+using DataAccess;
 
 namespace BusinessRules
 {
-    class BR_ListaNegra
+    public class BR_ListaNegra
     {
+        DA_ListaNegra daL = new DA_ListaNegra();
+        E_Mensaje msj = new E_Mensaje();
+
+        public DataTable ListarListaNegra()
+        {
+            DataTable tabla = new DataTable();
+            tabla = daL.ListarListaNegra();
+            return tabla;
+        }
+
     }
 }
--- Proyecto/DataAccess/DA_ListaNegra.cs
+++ Proyecto/DataAccess/DA_ListaNegra.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccess
 {
-    class DA_ListaNegra
+    public class DA_ListaNegra : DA_Connection
     {
+        public DataTable ListarListaNegra()
+        {
+            using (var connection = GetConnection())
+            {
+                DataTable tabla = new DataTable();
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "prc_ListarListaNegra";
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader leer = command.ExecuteReader();
+                    tabla.Load(leer);
+                    connection.Close();
+                    return tabla;
+
+                }    
+            }
+        }
+
+        public void AltaListaNegra (
     }
 }
