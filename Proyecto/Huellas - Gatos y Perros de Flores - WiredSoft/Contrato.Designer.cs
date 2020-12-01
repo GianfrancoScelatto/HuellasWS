6499217e571a83bb6bf7275d67f345eb20a72792
Rizzoli Matias <rizzolimatias@gmail.com>
2020-12-01 01:51:26 -0300

    Modulo Contrato

--- Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Contrato.Designer.cs
+++ Proyecto/Huellas - Gatos y Perros de Flores - WiredSoft/Contrato.Designer.cs
@@ -32,11 +32,10 @@
             this.cmbAnimal = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbPersona = new System.Windows.Forms.ComboBox();
-            this.btnGuardar = new System.Windows.Forms.Button();
             this.lblNuevoNombre = new System.Windows.Forms.Label();
-            this.txtNNombre = new System.Windows.Forms.TextBox();
-            this.dtpFechaAdopcion = new System.Windows.Forms.DateTimePicker();
-            this.label3 = new System.Windows.Forms.Label();
+            this.txtNuevoNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -77,23 +76,6 @@
             this.cmbPersona.Size = new System.Drawing.Size(190, 21);
             this.cmbPersona.TabIndex = 3;
             // 
-            // btnGuardar
-            // 
-            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGuardar.BackColor = System.Drawing.Color.White;
-            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGuardar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnGuardar.Location = new System.Drawing.Point(152, 279);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(170, 36);
-            this.btnGuardar.TabIndex = 89;
-            this.btnGuardar.Text = "Generar contrato";
-            this.btnGuardar.UseVisualStyleBackColor = false;
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
-            // 
             // lblNuevoNombre
             // 
             this.lblNuevoNombre.AutoSize = true;
@@ -105,48 +87,62 @@
             this.lblNuevoNombre.TabIndex = 90;
             this.lblNuevoNombre.Text = "Nuevo nombre";
             // 
-            // txtNNombre
+            // txtNuevoNombre
             // 
-            this.txtNNombre.Location = new System.Drawing.Point(261, 159);
-            this.txtNNombre.Name = "txtNNombre";
-            this.txtNNombre.Size = new System.Drawing.Size(190, 20);
-            this.txtNNombre.TabIndex = 91;
+            this.txtNuevoNombre.Location = new System.Drawing.Point(261, 159);
+            this.txtNuevoNombre.Name = "txtNuevoNombre";
+            this.txtNuevoNombre.Size = new System.Drawing.Size(190, 20);
+            this.txtNuevoNombre.TabIndex = 91;
             // 
-            // dtpFechaAdopcion
+            // btnGuardar
             // 
-            this.dtpFechaAdopcion.Location = new System.Drawing.Point(261, 224);
-            this.dtpFechaAdopcion.Name = "dtpFechaAdopcion";
-            this.dtpFechaAdopcion.Size = new System.Drawing.Size(200, 20);
-            this.dtpFechaAdopcion.TabIndex = 92;
+            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardar.BackColor = System.Drawing.Color.White;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGuardar.Location = new System.Drawing.Point(50, 208);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(165, 36);
+            this.btnGuardar.TabIndex = 92;
+            this.btnGuardar.Text = "Generar contrato";
+            this.btnGuardar.UseVisualStyleBackColor = false;
             // 
-            // label3
+            // btnCancelar
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(46, 223);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 21);
-            this.label3.TabIndex = 93;
-            this.label3.Text = "Fecha de Adopcion";
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCancelar.Location = new System.Drawing.Point(271, 208);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(166, 36);
+            this.btnCancelar.TabIndex = 93;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // Contrato
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
-            this.ClientSize = new System.Drawing.Size(545, 343);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.dtpFechaAdopcion);
-            this.Controls.Add(this.txtNNombre);
-            this.Controls.Add(this.lblNuevoNombre);
+            this.ClientSize = new System.Drawing.Size(477, 266);
+            this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNuevoNombre);
+            this.Controls.Add(this.lblNuevoNombre);
             this.Controls.Add(this.cmbPersona);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cmbAnimal);
             this.Controls.Add(this.label1);
             this.Name = "Contrato";
-            this.Text = "A";
+            this.Text = "Contrato";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,10 +154,9 @@
         private System.Windows.Forms.ComboBox cmbAnimal;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbPersona;
-        private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Label lblNuevoNombre;
-        private System.Windows.Forms.TextBox txtNNombre;
-        private System.Windows.Forms.DateTimePicker dtpFechaAdopcion;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNuevoNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
